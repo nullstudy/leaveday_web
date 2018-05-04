217806ddb9578022c0910c89e1ec4032c54bfd82
parkjuhuck <gusehdcks3@gmail.com>
2018-05-04 19:56:41 +0900

    jwt console add

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 export default async function({ isServer, store, route, redirect, req }) {
     if (isServer && !req) return 
     if (!store.getters.isAuthenticated) {
+        console.log(req)
         const token = isServer ? getUserFromCookie(req,store) : getUserFromLocalStorage()
         console.log('token',token)
         if (token) {
