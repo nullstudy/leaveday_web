be4c8d2ddc5e376e52449cffded7168ac2ad30fd
parkjuhuck <gusehdcks3@gmail.com>
2018-06-20 17:58:49 +0900

    # 16 다이어리 작성 UI 완성

--- pages/jobDiary/index.vue
+++ pages/jobDiary/index.vue
@@ -6,65 +6,55 @@
 
         <div class="bo_fx">
             <div id="bo_list_total"><span>Total 198,801건</span>1 페이지</div>
-            <li style="float:right; list-style:none;" class='bo_li'><a href="/diaryWrite" class="btn_b01" >글 작성</a></li> 
+            <li style="float:right; list-style:none;" class='bo_li'><a href="/jobDiary/create" class="btn_b01" >글 작성</a></li> 
         </div>
 
-        <table class ='table'>
+        <table class ='table table-striped'>
             <thead>
-            <tr>
-                <th>번호</th>
-                <th>제목</th>
-                <th>작성자</th>
-                <th>날짜</th>
-                <th>leavePoint</th>
-                <th>조회수</th>
-            </tr>
+                <tr>
+                    <th>번호</th>
+                    <th>제목</th>
+                    <th>작성자</th>
+                    <th>날짜</th>
+                    <th>상태</th>
+                    <th>조회수</th>
+                </tr>
             </thead>
 
             <tbody>
-                <tr v-for='(index,test ) in 25'>
+                <tr v-for='(index , test ) in 25' :key='test'>
                     <td>{{ index }}</td>
-                    <td><a href="/jobdiary" style='color:black; text-decoration: none;'>내 말을 끝까지 잘들어봐야 됩니다 정말로</a></td>
+                    <td><a href="/jobdiary" style='color:black; text-decoration: none;'>내 말을 끝까지</a></td>
                     <td>박주혁</td>
                     <td>2018.06.19</td>
-                    <td style='text-align:center right:10'>{{ Math.floor(Math.random() * 5 )}}</td>
-                    <td style='text-align:center right:10'>{{ Math.floor(Math.random() * 50000 )}}</td>
+                    <td style='text-align:center right:10'>화남</td>
+                    <td style='text-align:center right:10'>{{ Math.floor(Math.random() * 5000 )}}</td>
                 </tr>
             </tbody>
         </table>
         
-        <div class="pagination">
-            <a href="#">&laquo;</a>
-            <div v-for='( test,index ) in pageData' >
-                <a href="#"  
-                    v-bind:style="pageData[index].active ? {
-                        'background-color' : 'dodgerblue',
-                        'color': 'white'
-                    } : {} " 
-                    @click='classActive(index)' 
-                 >{{ index+1 }}</a>
-            </div>
-            <a href="#">&raquo;</a> 
-        </div>
+        <job-page :page='pageData' v-on:@change="onClickIndex" />
     </div>
 </template>
 
 <script>
+    import JobPage from '~/components/jobDiary/Pagination';
     export default {
+        components : {
+            JobPage
+        },
         data() {
             return {
-                pageData : [ { index : 0, active : true } ,{index : 1 ,active : false} , {index : 2 ,active : false}],
-                currentPage : 0,
-                isActive : false
+                pageData : [ { number : 0, active : true } ,{ number : 1 ,active : false} , { number : 2 ,active : false}],
+                currentPage : 0
             }
         },
-        methods:{
-            classActive(index){
-                this.pageData[this.currentPage].active = false;
-                this.currentPage = index;
-                this.pageData[index].active = true;
+        methods : {
+            onClickIndex(pageNumber) {
+                this.currentPage = pageNumber;
             }
         }
+        
     }
 </script>
 
@@ -76,29 +66,6 @@
     .breadcrumb{
         margin-top: 2%;
     }
-
-
-    .pagination {
-        align-items: center; 
-        justify-content: center;
-    }
-    /* Pagination links */
-    .pagination a {
-        color: black;
-        float: left;
-        padding: 8px 16px;
-        text-decoration: none;
-        transition: background-color .3s;
-    }
-
-    /* Style the active/current link */
-    /* .pagination a.active {
-        background-color: dodgerblue;
-        color: white;
-    } */
-
-    /* Add a grey background color on mouse-over */
-    .pagination a:hover:not(.active) {background-color: #ddd;}
     
     a.btn_b01 {
         display: inline-block;
--- pages/test.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="progress">
-        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
-        :style="{ width: 0.2 *100 +'%'}">
-            <!-- {{ quoteCount }} / {{ maxQuotes }}            -->
-        </div>
-    </div>
-</template>
