1319ff6ab169d408bb4563948466d08862ad5fed
parkjuhuck <gusehdcks3@gmail.com>
2018-06-27 23:16:15 +0900

    #9 TodoList 클릭이벤트 테스트 - reset , finish 클릭 (id값) 설정 확인

--- components/todo/ListComponent.vue
+++ components/todo/ListComponent.vue
@@ -4,6 +4,7 @@
       <li v-for="(item, index) in data" :key='index'>
         <span class="glyphicon glyphicon-asterisk"></span>
         {{item.todo}}
+        
         <span class="todoBtn label label-primary"
           v-if="selectedTab === 'todo' && item.status == false" 
           v-on:click="finishBtnClick(item,index)">
@@ -26,9 +27,11 @@ export default {
   props: ['data','i','selectedTab'],
   methods: {
     finishBtnClick(item) {
+      console.log('끝',item._id)
       this.$emit('@finish', item)
     },
     resetBtnClick(item) {
+      console.log('레셋',item._id)
       this.$emit('@reset', item)
     }
   },
