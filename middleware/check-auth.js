edc860286c5dacfe6dd3cafa75b8da08260f1669
parkjuhuck <gusehdcks3@gmail.com>
2018-07-23 15:37:24 +0900

    # aws 로그인테스트

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -6,6 +6,8 @@ import axios from 'axios'
 export default async function({  store, route, redirect, req }) {
     // console.log(isServer);
     if (process.server && !req) return;    
+
+    console.log('프로세스',process.server);
     const token = process.server ? getUserFromCookie(req,store) : getUserFromLocalStorage()
     token ? store.commit('SET_TOKEN', { token : token[0] }) : null
 }
