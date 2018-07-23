c21b98234ec7011a667e97f42cbd035809fa9ac3
parkjuhuck <gusehdcks3@gmail.com>
2018-07-23 15:59:54 +0900

    # aws 로그인 테스트

--- pages/index.vue
+++ pages/index.vue
@@ -4,20 +4,18 @@
       <!-- <leave-insert v-if="token && !isLeave"  v-b-modal.modal1 :userInfo="userInfo" /> -->
       
       <div class='main-content-profile'>
-        <main-banner :userInfo="userInfo" />
+        <!-- <main-banner :userInfo="userInfo" /> -->
       </div>
       <div class='main-content-calendar'>
-          <calendar />    
+          <!-- <calendar />     -->
       </div>
     </div>
 
 </template>
 
 <script>
-import { setToken } from '~/util/auth'
 import axios from 'axios';
 import { mapGetters, mapMutations, mapActions } from 'vuex'
-import { setCookie , getUserFromCookie } from '~/util/auth'
 import Calendar from '../components/Calendar'
 import LeaveInsert from "../components/LeaveInsert.vue"
 import MainBanner from '../components/MainBanner'
